eee2ddfd363026d31b48ff9676ab44070f7b7b04
Kholoud Mohamed <Kholoud.mohamed@hotmail.com>
2019-05-21 13:18:27 +0200

    Add specflow scenario

---
+++ SpecFlowRestAPITest/Features/Locations.feature
@@ -0,0 +1,18 @@
+﻿Feature: Search Locations
+	In order to improve my geographical info
+	As a user
+	I want to know the location details based on country and zipcode
+	
+@api
+Scenario Outline: Check location country name, placename, and state based on zip code and country abbreviation
+	Given I want to know the info of <countryAbb> with <zipCode>
+	When I retrieve the data of that location
+	Then Country name should be <countryName>
+    And Place name should be <placeName>
+    And State should be <state>
+Examples:
+|countryAbb |zipCode|countryName|placeName  |state          |
+|NL         |2274   |Netherlands|Voorburg   |Zuid-Holland   |
+|HR         |53296  |Croatia    |Metajna    |Ličko-Senjska  |
+
+
---
+++ SpecFlowRestAPITest/StepDefinitions/LocationsStepDef.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using SpecFlowRestAPITest.Objects;
+using SpecFlowRestAPITest.Utilities;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowRestAPITest.StepDefinitions
+{
+    [Binding]
+    public class LocationsStepDef
+    {
+        private RestClient restClient;
+        private RestRequest restRequest;
+        private IRestResponse restResponse;
+
+
+        [Given("I want to know the info of (.*) with (.*)")]
+        public void GivenIWantToKnowTheInfoOf(string countryAbb, string zipCode)
+        {
+            restClient = new RestClient(Data.ZipCodeBaseURL);
+            restRequest = new RestRequest("{country}/{zipcode}", Method.GET);
+
+            restRequest.AddUrlSegment("country", countryAbb);
+            restRequest.AddUrlSegment("zipcode", zipCode);
+        }
+
+        [When("I retrieve the data of that location")]
+        public void WhenIretrieveTheDataOfThatLocation()
+        {
+            restResponse = restClient.Execute(restRequest);
+        }
+
+        [Then("Country name should be (.*)")]
+        public void ThenCountryNameShouldBe(string countryName)
+        {
+            LocationResponse locationResponse = new JsonDeserializer().Deserialize<LocationResponse>(restResponse);
+            Assert.AreEqual(countryName, locationResponse.Country, "Country is not correct");
+
+        }
+
+        [Then(@"Place name should be (.*)")]
+        public void ThenPlaceNameShouldBe(string placeName)
+        {
+            LocationResponse locationResponse = new JsonDeserializer().Deserialize<LocationResponse>(restResponse);
+            Assert.AreEqual(placeName, locationResponse.Places[0].PlaceName, "Place Name is not correct");
+        }
+
+        [Then(@"State should be (.*)")]
+        public void ThenStateShouldBe(string state)
+        {
+            LocationResponse locationResponse = new JsonDeserializer().Deserialize<LocationResponse>(restResponse);
+            Assert.AreEqual(state, locationResponse.Places[0].State, "State is not correct");
+        }
+
+    }
+}
